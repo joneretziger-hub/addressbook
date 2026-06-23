753accf0ec1de4d1a991ccb3ed212e3263f772a4
Jone <joneretziger.com>
2026-06-23 16:30:54 +0530

    slave config

---
+++ server-script.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+sudo yum update -y
+sudo yum install java-1.8.0-openjdk-devel -y
+sudo yum install maven -y
+sudo yum install git -y 
+
+if [ -d "addressbook" ] 
+then
+  echo "repo is cloned and exists"\
+  cd /home/ec2-user/addressbook
+  git pull origin b1
+else
+  git clone git@github.com:joneretziger-hub/addressbook.git
+fi
+cd /home/ec2-user/addressbook
+
+mvn package

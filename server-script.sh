e66950b806fb0e9340ba94ab2ad64dea89cf4aea
Jone <joneretziger.com>
2026-07-06 15:38:58 +0530

    docker file modified

--- server-script.sh
+++ server-script.sh
@@ -7,15 +7,17 @@ sudo yum install git -y
 sudo yum install docker -y
 sudo systemctl enable docker
 sudo systemctl start docker 
-if [ -d "addressbook" ] 
+if [ -d "/home/ec2-user/addressbook" ] 
 then
   echo "repo is cloned and exists"\
   cd /home/ec2-user/addressbook
-  git fetch origin
+  git fetch --all
   git checkout docker-1
+  git reset --hard origin/docker-1
+  git clean -fd
   git pull origin docker-1
 else
-  git clone https://github.com/joneretziger-hub/addressbook.git
+  git clone https://github.com/joneretziger-hub/addressbook.git /home/ec2-user/addressbook
 fi
 cd /home/ec2-user/addressbook
 

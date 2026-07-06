b9bf928b07df5d4cf5611fe20c577101d039038d
Jone <joneretziger.com>
2026-07-06 16:53:22 +0530

    docker file modified

--- server-script.sh
+++ server-script.sh
@@ -14,7 +14,6 @@ then
   git fetch --all
   git checkout docker-1
   git reset --hard origin/docker-1
-  git pull origin docker-1
   git clean -fd
 else
   git clone https://github.com/joneretziger-hub/addressbook.git /home/ec2-user/addressbook
